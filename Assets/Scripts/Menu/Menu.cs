c7d3a10bff22cb6411351cb77930f630d091886b
Ganade <bganade@gmail.com>
2021-11-30 00:18:28 -0300

    Menu Quase completo

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Threading;
+
 
 public class Menu : MonoBehaviour
 {
     public string cena;
+    public string menu;
     public GameObject optionsPanel;
     public GameObject helpPanel;
 
@@ -19,16 +22,33 @@ public class Menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if( Input.GetKeyDown( KeyCode.X ) )
+             SceneManager.LoadScene(menu);
     }
 
     public void StartGame()
     {   
+        StartCoroutine(LoadingGame());
+    }
+    
+    IEnumerator LoadingGame()
+    {
+        yield return new WaitForSeconds(1);
         SceneManager.LoadScene(cena);
     }
 
     public void QuitGame()
     {
+        StartCoroutine(ExitGame());
+        //Editor Unity
+        UnityEditor.EditorApplication.isPlaying = false;
+        //Jogo Compilado
+        //Application.Quit();
+    }
+
+    IEnumerator ExitGame()
+    {
+        yield return new WaitForSeconds(1);
         //Editor Unity
         UnityEditor.EditorApplication.isPlaying = false;
         //Jogo Compilado
@@ -37,21 +57,48 @@ public class Menu : MonoBehaviour
 
     public void ShowOptions()
     {
+        StartCoroutine(ActiveOptions());
+    }
+
+    IEnumerator ActiveOptions()
+    {
+        yield return new WaitForSeconds(1);
         optionsPanel.SetActive(true);
     }
 
     public void BackToMenu()
+    {        
+        StartCoroutine(CloseOptions());
+    }
+
+    IEnumerator CloseOptions()
     {
+        yield return new WaitForSeconds(1);
         optionsPanel.SetActive(false);
     }
     
     public void ShowHelp()
     {
+        StartCoroutine(ActiveHelp());
+    }
+
+    IEnumerator ActiveHelp()
+    {
+        yield return new WaitForSeconds(1);
         helpPanel.SetActive(true);
     }
 
     public void BackToMenuHelp()
     {
+        StartCoroutine(CloseHelp());
+    }
+
+    IEnumerator CloseHelp()
+    {
+        yield return new WaitForSeconds(1);
         helpPanel.SetActive(false);
     }
+
+    
+
 }
